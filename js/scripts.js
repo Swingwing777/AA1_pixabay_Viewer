e2b76dcce0f7ede6c2f9c8ba1242892008b60ae0
David Hales <haldw@live.co.uk>
2020-08-08 13:45:24 +0400

    Working App with Conventional fetch
    Working if simple App with Conventional fetch.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 
 var photoRepository = (function () {
   var photoAlbum = [];
-  var apiUrl = 'https://pixabay.com/api/?key=17795524-3cd93801424773114b97b5b02&q=landscape+monochrome&image_type=photo';
+  var apiUrl =
+    'https://pixabay.com/api/?key=17795524-3cd93801424773114b97b5b02&q=landscape+monochrome&image_type=photo';
   var banner = $('.dataLoading');
   var modalContainer = $('#modal-container');
 
@@ -21,8 +22,11 @@ var photoRepository = (function () {
   function addListItem(photo) {
     var photoList = $('.photo-list');
     var photoItem = $('<li></li>');
-    var button = $('<button class="photoButton">Pixabay Photo ID + photo.pixID</button>');
-    var thumbNail = $('<img class="thumb" src=photo.preview>');
+    var button = $(
+      `<button style="font-size: 14px" class="photoButton">Pixabay ID: ${photo.pixID}</button>`
+    );
+    var thumbNail = $(
+      `<img style="height:70px; margin: 0 auto" class="thumb" src="${photo.preview}">`);
     button.append(thumbNail);
     photoItem.append(button);
     photoList.append(photoItem);
@@ -42,27 +46,28 @@ var photoRepository = (function () {
   //fetch photo data - conventional JavaScript
     function loadList() {
      showLoadingMessage(banner);
-     return fetch(apiUrl).then(function (response) {
-       return response.json();
-     }).then(function (json) {
-       json.hits.forEach(function (hit) {
-         console.log(hit.id, hit.tags, hit.previewURL, hit.webformatURL, hit.largeImageURL);
-         console.log(json);
-         var photo = {
-           pixID: hit.id,
-           tags: hit.tags,
-           preview: hit.previewURL,
-           webSize: hit.webformatURL,
-           image: hit.largeImageURL,
-           pageURL: hit.pageURL
+     return fetch(apiUrl)
+       .then(function (response) {
+         return response.json();
+       })
+       .then(function (json) {
+         json.hits.forEach(function (hit) {
+           var photo = {
+             pixID: hit.id,
+             tags: hit.tags,
+             preview: hit.previewURL,
+             webSize: hit.webformatURL,
+             largeImage: hit.largeImageURL,
+             pageURL: hit.pageURL,
          };
          add(photo);
          hideLoadingMessage(banner);
        });
-     }).catch(function (e) {
+     })
+     .catch(function (e) {
        hideLoadingMessage(banner);
        console.error(e);
-     })
+     });
    }
 
   // Ajax function - jQuery
@@ -73,14 +78,14 @@ var photoRepository = (function () {
     }).then(function (data){
       if (parseInt(data.totalHits) > 0)
       $.each(data.hits, function(i, hit){
-        console.log(hit.id, hit.tags, hit.previewURL, hit.webformatURL, hit.largeImageURL);
+        console.log(hit.id, hit.tags, hit.previewURL, hit.webformatURL, hit.largeImageURL, hit.pageURL);
         var photo = {
           pixID: hit.id,
           tags: hit.tags,
           preview: hit.previewURL,
           webSize: hit.webformatURL,
-          image: hit.largeImageURL,
-          pageURL: hit.pageURL
+          largeImage: hit.largeImageURL,
+          pageURL: hit.pageURL,
         };
         add(photo);
         //  hideLoadingMessage(banner);
@@ -94,8 +99,16 @@ var photoRepository = (function () {
 
 
   function showDetails(photo) {
+    console.log(photo);
     showLoadingMessage(banner);
-    showModal(photo.image, photo.tags, photo.pixID, photo.webSize, photo.pageURL);
+    showModal(
+      photo.preview,
+      photo.tags,
+      photo.pixID,
+      photo.webSize,
+      photo.largeImage,
+      photo.pageURL,
+    );
     hideLoadingMessage(banner);
   }
 
@@ -103,45 +116,48 @@ var photoRepository = (function () {
 //creates event listener for each button
 
   function buttonListen(button, photo) {
-    $('modalContainer').on("click", 'button', function(e) {
-      button.on('click', function (event) {
-        showDetails(photo);
-      });
+    button.on('click', function (event) {
+      showDetails(photo);
     });
-  };
+  }
 
   // --- Modal Functions within Photo Repository --------------
 
-  function showModal(img, tags, pixID, large, photoPage) {
+  function showModal(preview, tags, pixID, webSize, largeImage, pageURL) {
 
-    modalContainer.innerHTML = '';
+    modalContainer.innerHTML = ("");
     var modal = $('<div class="modal"></div>');
-    var imgElement = $('<img class="photoImage" alt="thumbnail" src = img>');
-    var titleElement = $('<h1 class="modalTitle">tags</div>');
-    var listElement = $('<ul class="photoDetail"></ul>');
-    var listElement__Item1 = $('<li class="photoID">pixID</li>');
-    var listElement__Item2 = $('<li class="photoTags">large</li>');
-    var listElement__Item3 = $('<li class="photoPage">photoPage"</li>');
+    var imgElement = $(
+      `<img style="max-width:700px; display:grid" class="photoImage" alt="thumbnail" src ="${webSize}">`
+    );
+    var titleElement = $(`<h1 class="modalTitle">Search Tags: ${tags}</div>`);
+    var listElement = $(`<ul class="photoDetail"></ul>`);
+    var imageLink = $(`<a href="${largeImage}" target="_blank">View fullsize image<a>`);
+    var pageLink = $(`<a href="${pageURL}" target="_blank">Leave Comment<a>`)
+    var listElement__Item1 = $(`<li style="color:white" class="photoID">Pixabay ID: ${pixID}</li>`);
+    var listElement__Item2 = $('<li class="photoHD"></li>');
+    var listElement__Item3 = $(`<li class="photoComment"></li>`);
 
     //create close button
     var closeButtonElement = $('<button class="modal-close">Close</button>');
-    $(closeButtonElement.on('click', hideModal));
-
-    $('listElement')
-    .append(listElement__Item1)
-    .append(listElement__Item2)
-    .append(listElement__Item3);
-    $('modal')
-    .append(titleElement)
-    .append(closeButtonElement)
-    .append(listElement)
-    .append(imgElement);
-    $('modalContainer').append(modal);
+    closeButtonElement.on('click', hideModal);
+    listElement__Item2.append(imageLink);
+    listElement__Item3.append(pageLink);
+    listElement               // Already jQuery defined.  No need for further $
+      .append(listElement__Item1)
+      .append(listElement__Item2)
+      .append(listElement__Item3);
+    modal                     //ditto
+      .append(titleElement)
+      .append(closeButtonElement)
+      .append(listElement)
+      .append(imgElement);
+    modalContainer.append(modal);   //ditto
 
     //focus closeButton so that user can simply press Enter
     closeButtonElement.focus();
 
-    $('modalContainer').addClass('is-visible');
+    modalContainer.addClass('is-visible');
   }
 
 
@@ -177,8 +193,10 @@ var photoRepository = (function () {
 
 // ------------- Functions external to IIFE -----------------------
 
-photoRepository.loadList().then(function(photo) {            // this calls the data from API and then calls getAll
-  photoRepository.getAll().forEach(function(photo){     //  getAll returns photo, followed by forEach loop, add to photoList array
+photoRepository.loadList().then(function(photo) {
+  // this calls the data from API and then calls getAll
+  photoRepository.getAll().forEach(function(photo){
+    //  getAll returns photo, followed by forEach loop, add to photoList array
     photoRepository.addListItem(photo);
   });
 });
